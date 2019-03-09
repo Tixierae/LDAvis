95e83bf89132cac8acbe9f2715e29bf35d92b721
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2019-03-09 17:14:54 +0100

    Update ldavis.js

--- inst/htmljs/ldavis.js
+++ inst/htmljs/ldavis.js
@@ -32,8 +32,9 @@ LDAvis = function(to_select, json_file) {
         current: 1
     },
 	
+	
     color1 = "#337ab7", // baseline color for default topic circles and overall term frequencies
-    color2 = "#f762f4"; // 'highlight' color for selected topics and term-topic frequencies
+    color2 = "#ffd300"; // 'highlight' color for selected topics and term-topic frequencies
 
     // Set the duration of each half of the transition:
     var duration = 750;
